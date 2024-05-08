29d2d2ee52bcf1276f0e9d36898aac5d95026939
vivek <vivekv290100@gmail.com>
2024-05-08 12:46:49 +0530

    11th

--- TEST.js
+++ TEST.js
@@ -1,1 +1,32 @@
 
+// function quick(arr=[34,56,7,6,56,54,46,46,45]){
+//     if(arr.length<=1){
+//         return arr
+//     }
+
+//     let pivot = arr[0]
+//     let left = []
+//     let right = []
+
+//     for(let i=1;i<arr.length;i++){
+//         if(arr[i]<pivot){
+//             left.push(arr[i])
+//         }else{
+//             right.push(arr[i])
+//         }
+//     }
+
+//     return [...quick(left), pivot, ...quick(right)]
+// }
+
+// console.log(quick());
+
+
+
+
+
+
+
+
+
+
